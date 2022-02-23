f26100a0818b1963f2cd820335c4cb84bea98bfa
Ren Wang <iamwrm@gmail.com>
2022-02-23 11:12:25 +0800

    add bash_eval_path

--- cli_common/src/lib.rs
+++ cli_common/src/lib.rs
@@ -1,5 +1,15 @@
+use anyhow::Result;
+use std::process::Command;
+
 fn print_a() {
     println!("a");
+}
+
+pub fn bash_eval_path(path: &str) -> Result<String> {
+    let bash_to_exec = format!("echo {}", path);
+    let bash_output = Command::new("bash").arg("-c").arg(bash_to_exec).output()?;
+    let after_sub = String::from_utf8(bash_output.stdout)?;
+    Ok(after_sub.trim().to_string())
 }
 
 #[test]
